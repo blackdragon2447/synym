7c43ef33fe224b6a2fd5b7f6b4c72c7090781400
BlackDragon2447 <blackdragon2447@e.email>
2024-09-07 15:52:34 +0200

    Inline assembly, no need for ".S" files

--- flake.nix
+++ flake.nix
@@ -24,24 +24,16 @@
           (with pkgs; [
             qemu
             rustup
-            just
           ])
           ++ (with crossPkgs; [clang gdb]);
-        shellHook = ''
-          export CC="riscv64-none-elf-cc -mabi=lp64d"
-        '';
       };
       devShells.gcc = pkgs.mkShell {
         nativeBuildInputs =
           (with pkgs; [
             qemu
             rustup
-            just
           ])
           ++ (with crossPkgs; [gcc gdb]);
-        shellHook = ''
-          export CC="riscv64-none-elf-gcc -mabi=lp64d"
-        '';
       };
       devShells.default = devShells.llvm;
     });
--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -1,7 +1,8 @@
+#![feature(naked_functions)]
 #![no_std]
 #![no_main]
 
-use core::panic::PanicInfo;
+use core::{arch::asm, panic::PanicInfo};
 
 mod sbi;
 
@@ -10,8 +11,36 @@ fn panic_handler(_: &PanicInfo) -> ! {
     loop {}
 }
 
+#[naked]
+#[link_section = ".text.init"]
 #[no_mangle]
-unsafe extern "C" fn kernel_main(_arg0: usize) -> ! {
+unsafe extern "C" fn _start() -> ! {
+    asm!(
+        ".option push",
+        ".option norelax",
+        "   la      gp, __global_pointer$",
+        "   la      t3, _bss_start",
+        "   la      t4, _bss_end",
+        ".option pop",
+        "1:",
+        "   bge     t3, t4, 1f",
+        "   sd      zero, 0(t3)",
+        "   addi    t3, t3, 8",
+        "   j       1b",
+        "1:",
+        "   la      sp, _stack_end",
+        "   li      t0, (1 << 8) | (1 << 5) | (1 << 13)",
+        "   csrw    sstatus, t0",
+        "   la      t1, kinit",
+        "   csrw    sie, zero",
+        "   csrw    sepc, t1",
+        "   sret",
+        options(noreturn)
+    )
+}
+
+#[no_mangle]
+unsafe extern "C" fn kinit(_arg0: usize) -> ! {
     let _ = sbi::debug_console::console_write("\nHello World!");
     loop {}
 }
